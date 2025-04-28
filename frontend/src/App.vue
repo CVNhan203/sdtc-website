bd92e8c5414009e30c1e5e341686004aac9745d5
CVNhan203 <cvnhan158203@gmail.com>
2025-04-28 10:17:00 +0700

    update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,31 @@
 <template>
+   <div id="app">
+    <!-- Header ở trên cùng-->
+  <Header />
   
+  <!-- Nội dung chính của các trang sẽ được hiển thị thông qua router-view -->
+  <main> 
+    <router-view /> 
+  </main>
+
+  <!-- Footer ở dưới cùng-->
+    <Footer />
+  </div>
+
 </template>
 
 <script>
 
+import Header from "./components/ComHeader.vue";
+import Footer from "./components/ComFooter.vue";
+
 export default {
   name: 'App',
- 
-}
+  components: {
+    Header,
+    Footer,
+  },
+};
 </script>
 
 <style>
@@ -20,6 +38,8 @@ export default {
   margin-top: 60px;
 }
 .body{
+  margin: 0;
+  padding: 0;
   
 }
 </style>
