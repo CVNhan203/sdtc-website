d345e08455d200f2aa1d971b06703b7e26f7478f
CVNhan203 <cvnhan158203@gmail.com>
2025-05-02 20:36:51 +0700

    update

---
+++ frontend/src/components/view/ComPriceList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <h2>Chào mừng đến </h2>
+    <p>Đây là phần nội dung.</p>
+  </div>
+</template>
+<script>
+export default {
+    name: 'ComPriceList',
+    data() {
+        return {
+            // Define your data properties here
+        };
+    },
+}
+</script>
+<style scoped>
+</style>
