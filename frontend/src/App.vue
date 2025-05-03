2adb86c972c11f6fbd5e5d0069c5aa935b60a25d
Sanhdepchoai <hotansanh0304@gmail.com>
2025-05-03 09:45:32 +0700

    fix admin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,4 @@
 <template>
-<<<<<<< HEAD
-   <div id="app">
-    <!-- Header ở trên cùng-->
-  <Header />
-  
-  <!-- Nội dung chính của các trang sẽ được hiển thị thông qua router-view -->
-  <main> 
-    <router-view /> 
-  </main>
-
-  <!-- Footer ở dưới cùng-->
-    <Footer />
-  </div>
-
-</template>
-
-<script>
-
-import Header from "./components/ComHeader.vue";
-import Footer from "./components/ComFooter.vue";
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.body{
-  margin: 0;
-  padding: 0;
-  
-=======
   <div id="app">
     <router-view></router-view>
   </div>
@@ -50,15 +6,15 @@ export default {
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap");
 
 body {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   margin: 0;
   padding: 0;
   background: rgba(248, 247, 250, 1);
@@ -67,6 +23,5 @@ body {
 #app {
   width: 100%;
   min-height: 100vh;
->>>>>>> origin/huynhFE
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-<<<<<<< HEAD
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-createApp(App).mount('#app')
-=======
-import router from './router'
-import '@fortawesome/fontawesome-free/css/all.css'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
->>>>>>> origin/huynhFE
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
