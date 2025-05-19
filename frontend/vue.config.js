99256d27fb8032fa069903fadb109aa3baa1b979
Sanhdepchoai <hotansanh0304@gmail.com>
2025-05-19 15:04:40 +0700

    Final changes before merging to main

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // Cấu hình đường dẫn cơ sở cho GitHub Pages
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/sdtc-website/' 
+    : '/',
+  // Thư mục output khi build
+  outputDir: 'dist'
 })
